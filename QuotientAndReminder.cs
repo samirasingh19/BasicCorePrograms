81a32cf9e76d59d88b3f6132a6a471b9581ec1a1
samirasingh19 <samira.singh021@gmail.com>
2023-03-15 20:11:34 +0530

    [200~ QuotientAndReminder

---
+++ QuotientAndReminder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class QuotientAndReminder
+    {
+        public static void FindQuotientAndReminder()
+        {
+            int dividend = 35, divisor = 5;
+
+
+                int reminder = dividend % divisor;
+            int Quotient = dividend / divisor;
+
+            Console.WriteLine("The dividend {0} and dicisor {1} ", dividend, divisor);
+            Console.WriteLine("The quotient is " + Quotient + " The reminder is " + reminder);
+        }
+    }
+}
