afa1e418fe9bd6f1d52e036ff4e6d1bfb15299a4
samirasingh19 <samira.singh021@gmail.com>
2023-03-15 20:07:57 +0530

    BasicCorePrograms

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.NetworkInformation;
+
+namespace BasicCorePrograms
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //Vowel_Consonant.CheckVowelAndConsonant();
+            //SwapTwoNumbers.DisplaySwapedNumbers();
+          //LargestNumber.FindLargest();
+            //QuotientAndReminder.FindQuotientAndReminder();
+        }
+    }
+}
